4b571d180eab2583653f25f55e2c2dafba8c3cf8
Aman Priyadarshi <85997402+Aman9955@users.noreply.github.com>
2023-10-28 22:03:35 +0530

    Update quick_sort.js

--- scripts/quick_sort.js
+++ scripts/quick_sort.js
@@ -1,9 +1,3 @@
-/*
-    *****************
-    DONE BY:-   TUMMALA KETHAN
-    
-    *****************
-*/
 
 function Quick()
 {
@@ -31,37 +25,37 @@ function quick_partition (start, end)
            
             if (div_sizes[ j ] < piv)
             {
-                div_update(divs[j],div_sizes[j],"yellow");//Color update
+                div_update(divs[j],div_sizes[j],"yellow");
 
-                div_update(divs[i],div_sizes[i],"red");//Color update
-                div_update(divs[j],div_sizes[j],"red");//Color update
+                div_update(divs[i],div_sizes[i],"red");
+                div_update(divs[j],div_sizes[j],"red");
 
                 var temp=div_sizes[i];
                 div_sizes[i]=div_sizes[j];
                 div_sizes[j]=temp;
 
-                div_update(divs[i],div_sizes[i],"red");//Height update
-                div_update(divs[j],div_sizes[j],"red");//Height update
+                div_update(divs[i],div_sizes[i],"red");
+                div_update(divs[j],div_sizes[j],"red");
 
-                div_update(divs[i],div_sizes[i],"blue");//Height update
-                div_update(divs[j],div_sizes[j],"blue");//Height update
+                div_update(divs[i],div_sizes[i],"blue");
+                div_update(divs[j],div_sizes[j],"blue");
 
                 i += 1;
             }
     }
-    div_update(divs[start],div_sizes[start],"red");//Color update
-    div_update(divs[i-1],div_sizes[i-1],"red");//Color update
+    div_update(divs[start],div_sizes[start],"red");
+    div_update(divs[i-1],div_sizes[i-1],"red");
     
     var temp=div_sizes[start];//put the pivot element in its proper place.
     div_sizes[start]=div_sizes[i-1];
     div_sizes[i-1]=temp;
 
-    div_update(divs[start],div_sizes[start],"red");//Height update
-    div_update(divs[i-1],div_sizes[i-1],"red");//Height update
+    div_update(divs[start],div_sizes[start],"red");
+    div_update(divs[i-1],div_sizes[i-1],"red");
 
     for(var t=start;t<=i;t++)
     {
-        div_update(divs[t],div_sizes[t],"green");//Color update
+        div_update(divs[t],div_sizes[t],"green");
     }
 
     return i-1;//return the position of the pivot
@@ -73,14 +67,7 @@ function quick_sort (start, end )
     {
         //stores the position of pivot element
         var piv_pos = quick_partition (start, end ) ;     
-        quick_sort (start, piv_pos -1);//sorts the left side of pivot.
-        quick_sort (piv_pos +1, end) ;//sorts the right side of pivot.
+        quick_sort (start, piv_pos -1);
+        quick_sort (piv_pos +1, end) ;
     }
  }
-
-/*
-    *****************
-    DONE BY:-   TUMMALA KETHAN
-    
-    *****************
-*/
